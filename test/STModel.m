a552f743ad0cdb8d41f264f82af35cc880aadf57
Sulee <slccper@gmail.com>
2016-04-08 09:57:23 +0800

    一些修改
    增加更安全的方式

--- test/STModel.m
+++ test/STModel.m
@@ -63,6 +63,35 @@
     }
     return [self getDic:obj];
 }
++ (void) ObjectSetValue:(NSObject *)objinstance : (NSString *)propertyname  :(NSString *)value{
+    const NSSet *boolTypes = [NSSet setWithObjects: @"Tc", nil];
+    const NSSet *intTypes  = [NSSet setWithObjects: @"Ti", nil];
+    const NSSet *floatTypes = [NSSet setWithObjects: @"Tf", nil];
+    const NSSet *doubleTypes = [NSSet setWithObjects: @"Td", nil];
+    const NSSet *stringTypes = [NSSet setWithObjects: @"T@\"NSString\"", nil];
+    
+    objc_property_t otype=class_getProperty([objinstance class], [propertyname UTF8String]);
+    NSString *dataType = [NSString stringWithUTF8String: property_getAttributes(otype)];
+    dataType=([dataType componentsSeparatedByString:@","])[0];
+    
+    if ([stringTypes containsObject: dataType]) {
+        [objinstance setValue:value forKey:propertyname];
+    }if ([boolTypes containsObject: dataType]) {
+        BOOL blnvalue=[value isEqualToString:@"false"] ? NO : YES;
+        [objinstance setValue: [NSNumber numberWithBool:blnvalue] forKey:propertyname];
+    }else if ([intTypes containsObject: dataType]) {
+        int intvalue=[value integerValue];
+        [objinstance setValue: [NSNumber numberWithInt:intvalue] forKey:propertyname];
+    }else if ([floatTypes containsObject: dataType]) {
+        float floatvalue=[value floatValue];
+        [objinstance setValue: [NSNumber numberWithFloat:floatvalue] forKey:propertyname];
+    }else if ([doubleTypes containsObject: dataType]) {
+        double doublevalue=[value doubleValue];
+        [objinstance setValue: [NSNumber numberWithDouble:doublevalue] forKey:propertyname];
+    }else{
+        [objinstance setValue:value forKey:propertyname];
+    }
+}
 + (id)initWithDic:(NSDictionary *)dic :(id)obj{
     unsigned int count;
     objc_property_t *propertys = class_copyPropertyList([obj class], &count);
@@ -72,16 +101,47 @@
         NSString *propertyName = [NSString stringWithUTF8String:name];
         id value = [NSString stringWithFormat:@"%@",dic[propertyName]];
         if(value == nil){
-            value = [NSNull null];
+            value = @"";
         }else{
             value = [self getObjectInternal:value];
         }
-        SEL selector =[self creatSetterWithPropertyName:propertyName];
-        IMP imp = [obj methodForSelector:selector];
-        void (*func)(id,SEL,NSString *)= (void *)imp;
-        func(obj,selector,value);
+        [self ObjectSetValue:obj :propertyName :value];
+//        SEL selector =[self creatSetterWithPropertyName:propertyName];
+//        IMP imp = [obj methodForSelector:selector];
+//        void (*func)(id,SEL,NSString *)= (void *)imp;
+//        func(obj,selector,value);
     }
     free(propertys);
+    return obj;
+}
++(id)getObject:(id)obj{
+    if([obj isKindOfClass:[NSString class]]
+       || [obj isKindOfClass:[NSNumber class]])
+    {
+        return obj;
+    }
+    
+    if([obj isKindOfClass:[NSArray class]])
+    {
+        NSArray *objarr = obj;
+        NSMutableArray *arr = [NSMutableArray arrayWithCapacity:objarr.count];
+        for(int i = 0;i < objarr.count; i++)
+        {
+            [arr setObject:[self getObject:[objarr objectAtIndex:i]] atIndexedSubscript:i];
+        }
+        return arr;
+    }
+    
+    if([obj isKindOfClass:[NSDictionary class]])
+    {
+        NSDictionary *objdic = obj;
+        NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithCapacity:[objdic count]];
+        for(NSString *key in objdic.allKeys)
+        {
+            [dic setObject:[self getObject:[objdic objectForKey:key]] forKey:key];
+        }
+        return dic;
+    }
     return obj;
 }
 +(SEL)creatSetterWithPropertyName: (NSString *) propertyName{
